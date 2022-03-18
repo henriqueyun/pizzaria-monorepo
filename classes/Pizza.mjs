e645cb21d6cd616cbd67c518a74e9942b54c2be6
henriqueyun <henriqueyun@gmail.com>
2022-03-18 11:55:21 -0300

    removendo sujeira do git :S

--- classes/Pizza.mjs
+++
@@ -1,9 +0,0 @@
-import Produto from './Produto.mjs'
-
-export default class Pizza extends Produto {
-  constructor(nome, preco, imgURL, ingredientes, saborAdicional) {
-    super(nome, preco, imgURL)
-    this.ingredientes = ingredientes
-    this.saborAdicional = saborAdicional
-  }
-}
--- classes/Produto.mjs
+++
@@ -1,7 +0,0 @@
-export default class Produto {
-  constructor(nome, preco, imgURL) {
-    this.nome = nome
-    this.preco = preco
-    this.imgURL = imgURL
-  }
-}
