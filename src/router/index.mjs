9886d7d3714be2b61eadb752410515f0c97d3743
henriqueyun <henriqueyun@gmail.com>
2022-03-23 19:36:51 -0300

    adicionando rotas de busca para todas as pizzs e bebidas

--- src/router/index.mjs
+++ src/router/index.mjs
@@ -4,11 +4,13 @@ import controller from '../controller/index.mjs'
 const router = Router()
 
 router.post('/pizza', controller.pizzaController.cadastrar)
+router.get('/pizza/all', controller.pizzaController.buscarTodas)
 router.get('/pizza/:id', controller.pizzaController.buscar)
 router.put('/pizza/:id', controller.pizzaController.editar)
 router.delete('/pizza/:id', controller.pizzaController.excluir)
 
 router.post('/bebida', controller.bebidaController.cadastrar)
+router.get('/bebida/all', controller.bebidaController.buscarTodas)
 router.get('/bebida/:id', controller.bebidaController.buscar)
 router.put('/bebida/:id', controller.bebidaController.editar)
 router.delete('/bebida/:id', controller.bebidaController.excluir)
