2756e26b1cefa53ae08f5fe2d7df7d19eee3a73c
henriqueyun <henriqueyun@gmail.com>
2022-05-18 19:22:14 -0300

    fix: ajustando o nome da variável de entrada para entradas

--- src/services/PedidoService.js
+++ src/services/PedidoService.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 async function buscarTodosPorStatus () {
   const pedidosResponse = await axios.get(`${process.env.VUE_APP_API_URL}/pedido/all`)
   const pedidos = pedidosResponse.data
-  const entrada = pedidos.filter(pedido => pedido.status == 'entrada')
+  const entradas = pedidos.filter(pedido => pedido.status == 'entrada')
   const confirmados = pedidos.filter(pedido => pedido.status == 'confirmado')
   const emPreparo = pedidos.filter(pedido => pedido.status == 'preparo')
   const expedidos = pedidos.filter(pedido => pedido.status == 'expedido')
   
-  return { entrada, confirmados, emPreparo, expedidos }
+  return { entradas, confirmados, emPreparo, expedidos }
 }
 
 async function atualizarStatusPedido (pedidoId, status) {
