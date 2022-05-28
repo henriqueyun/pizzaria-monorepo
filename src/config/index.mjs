e8895565661e25c1c370cae3d82ecca1c749c442
Henrique Lima <henriqueyun@gmail.com>
2022-05-27 23:50:32 -0300

    Dev (#2)
    * portas padrão setadas para 8010 e adicionado env_file no compose do app
    * adicionado script start no package.json

--- src/config/index.mjs
+++ src/config/index.mjs
@@ -5,7 +5,7 @@ const config = {
   databaseHost: process.env.DB_HOST || 'localhost',
   databasePort: process.env.DB_PORT || 3306,
   logLevel: process.env.LOG_LEVEL || 'debug',
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8010
 }
 
 export default config
