9a214614895a2211763939f6aa34371cee40badc
henriqueyun <henriqueyun@gmail.com>
2025-02-20 14:07:25 -0300

    fix: loading while the request

--- frontend-pizzaria-cliente/src/views/WelcomeView.vue
+++ frontend-pizzaria-cliente/src/views/WelcomeView.vue
@@ -56,6 +56,7 @@ export default {
     },
     async healthCheck () {
       try {
+        this.isLoading = true
         await HealthService.checarHealth()
         this.isAPIOnline = true
       } catch (error) {
